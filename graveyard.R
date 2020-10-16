5ef4e6aa9765446fbf4f2b363fe2060f2d686dd4
Ivan Ropovik <ivan.ropovik@gmail.com>
2020-10-16 21:25:58 +0200

    backup

--- graveyard.R
+++ graveyard.R
@@ -2,6 +2,18 @@
 #########
 # Graveyard
 #########
+
+#'## Bayesian analysis of predictive power
+#'
+#' **For achievement measures**
+#'
+#' **Bayes factor in favor of the alternative hypothesis (BF10) and posterior probability for model parameters (given 1:1 prior odds for H0:Ha)**
+#'
+#'Bayes factors show whether there is evidence either for Ha (effect present) or H0 (effect absent), i.e., whether the data are more consistent with Ha, H0, or inconclusive. Posterior probability refers to the probability of the *parameter* not being zero (as oposed to probability of the data under a null).
+#'Frequentist approach without specific procedures (like equivalence testing), on the other hand, cannot provide evidence for H0, by definition (the only possible conclusions are H0 being rejected or failed to be rejected).
+#'These are Bayes Factors based on model selection / information criteria approach as proposed by Wagenmakers, 2007. Each BF represents the relative evidence in the data favoring alternative hypothesis (parameter freely estimated) over the null (the given parameter fixed to 0).
+#'Bayes Factors using BIC approximation implicitly assume unit information prior which makes them rather conservative with regard to the alternative hypothesis.
+
 #
 # # Base rato no of responses per perpetrators
 # baseWho <- data %>% select(contains("_who") & !contains("Other")) %>% map(~sum(!is.na(.))) %>% as.numeric(.) %>% .[1:120*6] %>% .[!is.na(.)] #Calculating the base rate no of responses per perpetrators and selecting every 6th element
